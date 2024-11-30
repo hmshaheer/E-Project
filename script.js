a738c8773dcb7b2f72410e05f77f2a6a9760da25
Hafiz Muhammmad Shaheer <hmshaheersohail@gmail.com>
2024-11-30 16:40:21 +0500

    Add files via upload
    I Don't Know

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    setTimeout(function () {
+
+        document.getElementById('preloader').style.display = 'none';
+
+        document.querySelector('.content').style.display = 'block';
+    }, 2000); 
+});
